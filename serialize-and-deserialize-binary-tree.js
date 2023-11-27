d1d319db8564a4bc77489bbc0aef6d88eebbe521
Nothing <wwwaap@gmail.com>
2023-11-27 14:41:23 +0800

    Update serialize-and-deserialize-binary-tree.js

--- serialize-and-deserialize-binary-tree.js
+++ serialize-and-deserialize-binary-tree.js
@@ -35,3 +35,42 @@ function deserialize(str) {
     return res
   }
 }
+
+// another
+
+/**
+ * @param {Node} root
+ * @return {string}
+ */
+function serialize(root) {
+  const q = [root];
+  const result = [];
+  while(q.length) {
+    const node = q.shift();
+    if(node) {
+      result.push(node.value);
+      q.push(node.left, node.right);
+    } else {
+      result.push('null');
+    }
+  }
+  return result.join(',');
+}
+
+/**
+ * @param {string} str
+ * @return {Node}
+ */
+function deserialize(str) {
+  const arr = str.split(',');
+  return buildTree(arr, 0);
+}
+
+function buildTree(arr, index) {
+  if(arr[index] === 'null') return null;
+  if(index >= arr.length) return null;
+  const node = new Node(arr[index]);
+  node.left = buildTree(arr, index * 2 + 1);
+  node.right = buildTree(arr, index * 2 + 2);
+  return node;
+}
