3d2ea1536102017be42e041d5194387e3e5fbff4
Nothing <wwwaap@gmail.com>
2024-10-17 15:23:06 +0800

    Create MultiDimensionalArray.js

---
+++ MultiDimensionalArray.js
@@ -0,0 +1,12 @@
+function MultiDimensionalArray(defaultValue, ...args) {
+  if (args.length === 1) {
+    return Array(args[0]).fill(defaultValue)
+  }
+  const res = []
+
+  for (let i = 0, n = args[0]; i < n; i++) {
+    res.push(MultidimensionalArray(defaultValue, ...args.slice(1)))
+  }
+
+  return res
+}
